0a710dd9cce793289c092e55f5e7e0cacc043622
SaadbinGhaffar <saadghaffar97@gmail.com>
2026-04-08 07:10:55 +0500

    added product schema

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Name field is Required"] },
+
+  price: {
+    type: Number,
+    required: [true, "Product Price Must be Provided"],
+  },
+
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: { type: Date, default: Date.now() },
+  company: {
+    type: String,
+
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is Not Supported",
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
